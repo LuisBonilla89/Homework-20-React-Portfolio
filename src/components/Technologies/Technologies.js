4c63787dafb5d4d7e2026e2417baa0a24a54b563
LuisBonilla89 <78412087+LuisBonilla89@users.noreply.github.com>
2021-08-30 18:26:20 +1000

    Adding technologies section and main portfolio body

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      <br></br>
+      Front-end Development: React, Semantic-UI, HTML, CSS, Bootstrap, JQuery,
+      Javascript <br></br>
+      <br></br>Back-end Development: ExpressJS, NodeJS, MongoDB, MySQL,GraphQL,
+      Apollo Serve, REST API <br></br>
+      <br></br>Computer Software: Visual Studio Code, Slack, Microsoft Office,
+      Git, Figma. <br></br>
+      <br></br>Front End Architecture: OOP(Object Oriented Programming), Model
+      View Controller (MVC) <br></br>
+      <br></br>Spoken Languages: Native Spanish Speaker, business level English
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js, MongoDB, MySQL, Apollo Server, GraphQL
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
