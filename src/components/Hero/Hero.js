89a6bf3997502f67a4a138dede1d07c7798dd556
LuisBonilla89 <78412087+LuisBonilla89@users.noreply.github.com>
2021-08-30 15:54:39 +1000

    Adding project section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Portfolio
+      </SectionTitle>
+      <SectionText>
+        Hello there! My name is Luis Bonilla. I am a web developer ready to
+        expand and develop a sucessful career in the tech world. Graduated from
+        Monash Coding Bootcam in Melbourne Australia in August 2021 and acquired
+        crucial knowledge in the latest industry technologies such as MERN stack
+        (MongoDB, Express, React, NodeJS) as well as knowledge in GraphQL,
+        Javascript, CSS, HTML, JQuery and Bootstrap.<br></br>
+        <br></br>
+        Acquiring this new skillset complements my long standing interest in the
+        power of programing. Please allow me to introduce some of my work. I
+        hope you enjoy the visit.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
